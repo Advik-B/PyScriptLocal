1e99af6e8b4085c8005f6c085dcd38efa03eb58a
Advik <advik.b@gmail.com>
2022-10-24 00:01:21 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,10 @@
 from sys import stdout
 
 
-def write(*string: str, flush: bool = False, sep: str = " ", end: str = "\r\n") -> None:
+def console_out(*string: str, flush: bool = False, sep: str = " ", end: str = "\r\n") -> None:
     for s in string:
         stdout.write(s)
         stdout.write(sep)
     stdout.write(end)
     if flush:
         stdout.flush()
-
-
-write("Hello World", "World")

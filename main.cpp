67dc62f8b5a94e11fb0481b44aa0ea1cd56ff82f
Advik <advik.b@gmail.com>
2022-10-24 00:43:28 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,18 @@
-#include "libconsole.hpp"
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::string;
+
+void print(const string text, bool flush = false, bool newline = true) {
+    for (char s: text){
+        cout << s;
+    }
+    if (newline) cout << "\r\n";
+    if (flush) cout.flush();
+}
+
+
 
 int main() {
     print("Hello, World!");

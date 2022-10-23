30e22450b46d0b9f0ec297cd9f4028a87758d481
Advik <advik.b@gmail.com>
2022-10-23 23:27:44 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-a = 10
-print(dir())
+from sys import stdout
+
+
+def write(*string: str, flush: bool = False, sep: str = " ", end: str = "\r\n") -> None:
+    for s in string:
+        stdout.write(s)
+        stdout.write(sep)
+    stdout.write(end)
+    if flush:
+        stdout.flush()
+
+
+write("Hello World", "World")

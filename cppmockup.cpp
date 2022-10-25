49098c7cf84b7e304580da54c0003d63f3ba6837
Advik <advik.b@gmail.com>
2022-10-25 17:11:59 +0530

    Update cppmockup.cpp

--- cppmockup.cpp
+++ cppmockup.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 
 using std::cout;
 using std::string;
 using std::vector;
+using std::endl;
+using std::ifstream;
+using std::getline;
+
+// using for file I/O
 
 void print(const string text, bool flush = false, bool newline = true, string endl="\n") {
     for (char s: text){
@@ -20,9 +26,21 @@ void print_vector(const vector<string> &v, bool flush = false, bool newline = tr
     }
 }
 
+vector<string> read_text_file(const string &filename) {
+    vector<string> lines;
+    string line;
+    ifstream file(filename);
+    while (getline(file, line)) {
+        lines.push_back(line);
+    }
+    return lines;
+}
+
 int main()
 {
-    string text {"A long time ago in a galaxy far, far away..."};
-    print(text);
+    // Read the bee movie script
+    vector<string> lines = read_text_file("bee_movie_script.txt");
+    print_vector(lines);
+    print("Done", true);
     return 0;
 }

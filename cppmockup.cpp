576487f7c8fa264f85c4b6452dfa485931ead6ec
Advik <advik.b@gmail.com>
2022-10-25 17:12:30 +0530

    Update cppmockup.cpp

--- cppmockup.cpp
+++ cppmockup.cpp
@@ -39,8 +39,7 @@ vector<string> read_text_file(const string &filename) {
 int main()
 {
     // Read the bee movie script
-    vector<string> lines = read_text_file("bee_movie_script.txt");
+    vector<string> lines = read_text_file("bee-movie-script.txt");
     print_vector(lines);
-    print("Done", true);
     return 0;
 }
